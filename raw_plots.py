954bddf0a112d60274f8ad4677072566b3b1e587
jesse <jesse.f@technotialabs.com>
2022-09-17 08:44:41 +1000

    raw data plots

---
+++ raw_plots.py
@@ -0,0 +1,73 @@
+import argparse
+from datetime import datetime
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from helpers import get_map
+
+YEARS = range(2000, 2022)
+DATA_DIR = 'data/precipitation'
+DATA_FILE = f'{DATA_DIR}/FusedData.csv'
+LOCATIONS_FILE = f'{DATA_DIR}/Fused.Locations.csv'
+size_func = lambda series: [500 * n for n in series]
+cmap = 'RdYlGn_r'
+
+def prepare_indexed_df(input_df, new_index='date'):
+    input_df.columns = pd.to_datetime(input_df.columns, format='D%Y.%m')
+    df_locations = pd.read_csv(LOCATIONS_FILE)
+    df = pd.concat([df_locations, input_df], axis=1)
+    df = df.set_index(['Lat', 'Lon'])
+    df = df.stack().reset_index()
+    df = df.rename(columns={'level_2': 'date', 0: 'prec', 'Lat': 'lat', 'Lon': 'lon'})
+    df = df.set_index(new_index)
+    return df
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output_folder', default=None)
+    parser.add_argument('--month', type=int, default=None)
+    args = parser.parse_args()
+
+    raw_df = pd.read_csv(DATA_FILE)
+    df = prepare_indexed_df(raw_df, ['lat', 'lon'])
+
+    pos = None
+    if args.month:
+        months = [args.month]
+    else:
+        months = range(1, 13)
+    for y in YEARS:
+        for m in months:
+            dt = datetime(y, m, 1)
+            dt_prec_df = df[df['date'] == dt]
+            if dt_prec_df.empty:
+                continue
+
+            figure, axis = plt.subplots(1)
+            _map = get_map(axis)
+            if pos is None:
+                lats, lons = zip(*dt_prec_df.index.values)
+                mx, my = _map(lons, lats)
+                pos = {elem: (mx[i], my[i]) for i, elem in enumerate(dt_prec_df.index)}
+
+            series = dt_prec_df['prec']
+            axis.scatter(
+                mx, my, c=series, cmap=cmap,
+                s=size_func((series - np.min(series)) / np.max(series)),
+            )
+            norm = mpl.colors.Normalize(vmin=np.min(series), vmax=np.max(series))
+            plt.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap), ax=axis)
+            plt.tight_layout()
+            axis.set_title(f'{dt.strftime("%b")} {y}: raw precipitation', fontsize=20)
+            if args.output_folder:
+                month_str = str(m) if m >= 10 else f'0{m}'
+                filename = f'raw_plot_{y}_{month_str}.png'
+                figure.set_size_inches(32, 18)
+                plt.savefig(f'{args.output_folder}/{filename}', bbox_inches='tight')
+                print(f'Plot saved to file {args.output_folder}/{filename}!')
+            else:
+                plt.show()
+            plt.close()
+
+main()
