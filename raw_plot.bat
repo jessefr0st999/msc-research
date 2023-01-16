54b0d6c3c4e98e1f73cbdec5faff1eb07b94fbaf
Jesse <jessefrost999@gmail.com>
2023-01-16 13:33:33 +1100

    remove fluff

--- raw_plot.bat
+++
@@ -1,7 +0,0 @@
-python raw_plots.py --output_folder images/video --month 3
-
-python build_video.py --target_folder images/video --video_name raw_plot_Mar
-
-move images\video\raw_plot_Mar.mp4 images\raw_plot_Mar.mp4
-
-del /S /Q images\video\*
