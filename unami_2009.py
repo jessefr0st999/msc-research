cb80d04185f2e559df485b0653b1e2b413e1f998
Jesse <jessefrost999@gmail.com>
2023-05-20 17:56:35 +1000

    various things fixed in unami but still not right

--- unami_2009.py
+++ unami_2009.py
@@ -6,13 +6,15 @@ import pandas as pd
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from scipy.sparse.linalg import spsolve
 
 from helpers import get_map, scatter_map, prepare_df, configure_plots
 
+np.random.seed(0)
 np.set_printoptions(suppress=True)
 YEARS = range(2000, 2022)
-SERIES_KEY = (-28.75, 153.5)
-OUTPUT_FILE = 'unami_2009_proc_data.csv'
+FUSED_SERIES_KEY = (-28.75, 153.5)
+BOM_DAILY_FILE = 'BOMDaily086213_rosebud'
 
 def dt_to_prev_month_days(dt: datetime):
     if dt.month in [5, 7, 10, 12]:
@@ -23,53 +25,105 @@ def dt_to_prev_month_days(dt: datetime):
         return 29 if dt.year % 4 == 0 else 28
     return 31
 
+def plot_data(model_df):
+    figure, axes = plt.subplots(3, 1)
+    axes = iter(axes.T.flatten())
+    axis = next(axes)
+    axis.plot(model_df['t'], model_df['s'], 'bo-')
+    axis.set_xlabel('t')
+    axis.set_ylabel('s')
+    
+    axis = next(axes)
+    axis.plot(model_df['t'], model_df['x'], 'ro-')
+    axis.set_xlabel('t')
+    axis.set_ylabel('x')
+
+    axis = next(axes)
+    axis.plot(model_df['t'], model_df['t_delta'], 'go-')
+    axis.set_xlabel('t')
+    axis.set_ylabel('t_delta')
+    plt.show()
+
+def plot_params(x_vec, param_func):
+    t_vec = range(365)
+    figure, axes = plt.subplots(3, 1)
+    axes = iter(axes.T.flatten())
+    axis = next(axes)
+    axis.plot(t_vec, [param_func('beta', t) for t in t_vec], 'bo-')
+    axis.set_xlabel('t')
+    axis.set_ylabel('beta')
+
+    axis = next(axes)
+    axis.plot(t_vec, [param_func('psi', t, x_vec.mean()) for t in t_vec], 'ro-')
+    axis.set_xlabel('t')
+    axis.set_ylabel('psi = log(v)')
+
+    axis = next(axes)
+    axis.plot(t_vec, [param_func('kappa', t) for t in t_vec], 'go-')
+    axis.set_xlabel('t')
+    axis.set_ylabel('kappa = log(K)')
+    plt.show()
+
+def prepare_model_df(prec_series, prec_inc):
+    cum_sums = prec_series.cumsum()
+    times = prec_series.index
+    # Calculate values of the temporal variable X
+    model_df_values = []
+    for i, s in enumerate(cum_sums):
+        # t_delta: at a given t, how far (in hours) do you have to look back until the
+        # difference surpasses prec_inc (delta in paper)
+        if s - cum_sums[0] < prec_inc:
+            continue
+        i_prev = i - 1
+        # Skip consecutive equal s-values
+        if s == cum_sums[i_prev]:
+            continue
+        while s - cum_sums[i_prev] < prec_inc:
+            i_prev -= 1
+        if i_prev == i - 1:
+            # Handle case where the threshold is exceeded by multiples of the jump on the latest
+            # timestamp by using a fraction of the jump between previous timestamps for t_delta
+            inc_jumps = int((s - cum_sums[i_prev]) // prec_inc)
+            t_delta = 24 * (times[i_prev + 1] - times[i_prev]).days / inc_jumps
+        else:
+            t_delta = 24 * (times[i] - times[i_prev]).days
+        # Used to prevent consecutive x-values being equal
+        t_delta += np.abs(np.random.normal(0, 6))
+        x = np.log(prec_inc / t_delta)
+        model_df_values.append((s, times[i], x, t_delta))
+    model_df = pd.DataFrame(model_df_values)
+    model_df.columns = ['s', 't', 'x', 't_delta']
+    return model_df
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_dir', default='data/precipitation')
-    parser.add_argument('--data_file', default='FusedData.csv')
+    parser.add_argument('--data_type', default='fused')
     parser.add_argument('--prec_inc', type=int, default=500)
     parser.add_argument('--x_steps', type=int, default=30)
     parser.add_argument('--t_steps', type=int, default=30)
     parser.add_argument('--s_steps', type=int, default=1000)
+    parser.add_argument('--delta_s', type=int, default=0.02)
     parser.add_argument('--calculate_model_df', action='store_true', default=False)
     parser.add_argument('--plot', action='store_true', default=False)
     args = parser.parse_args()
 
-    prec_df, lats, lons = prepare_df(args.data_dir, args.data_file, 'prec')
-    filename = f'{args.data_dir}/{OUTPUT_FILE}'
+    if args.data_type == 'fused':
+        prec_df, _, _ = prepare_df('data/precipitation', 'FusedData.csv', 'prec')
+        prec_series = prec_df[FUSED_SERIES_KEY]
+    elif args.data_type == 'bom_daily':
+        prec_df = pd.read_csv(f'data_unfused/{BOM_DAILY_FILE}.csv')
+        prec_df.index = pd.DatetimeIndex(prec_df['Date'])
+        # prec_series = pd.Series(prec_df['Rain']).dropna().loc['2000-04-01':]
+        prec_series = pd.Series(prec_df['Rain']).dropna().loc['2000-04-01':]
+    filename = f'data/precipitation/unami_2009_proc_delta_{args.prec_inc}.csv'
     if args.calculate_model_df:
-        cum_sums = prec_df[SERIES_KEY].cumsum()
-        times = prec_df.index.values
-        # Calculate values of the temporal variable X
-        model_df_values = []
-        for i, s in enumerate(cum_sums):
-            # t_delta: at a given t, how far (in hours) do you have to look back until the
-            # difference surpasses prec_inc (delta in paper)
-            if s - cum_sums[0] < args.prec_inc:
-                continue
-            i_prev = i - 1
-            while s - cum_sums[i_prev] < args.prec_inc:
-                i_prev -= 1
-            if i_prev == i - 1:
-                # Handle case where the threshold is exceeded by multiples of the jump on the
-                # latest timestamp by using a fraction of the time granularity for t_delta
-                inc_jumps = int((s - cum_sums[i_prev]) // args.prec_inc)
-                t_delta = (times[i_prev + 1] - times[i_prev]).days / inc_jumps
-                ######################################################
-                # print(times[i].strftime('%Y %m'), i, i - i_prev, round(s),
-                #     round(s - cum_sums[i_prev]), inc_jumps, round(t_delta, 2))
-                ######################################################
-            else:
-                t_delta = (times[i] - times[i_prev]).days
-            t_delta += np.random.normal(0, 10)
-            x = np.log(args.prec_inc / t_delta)
-            model_df_values.append((s, times[i], x, t_delta))
-        model_df = pd.DataFrame(model_df_values)
-        model_df.columns = ['s', 't', 'x', 't_delta']
+        model_df = prepare_model_df(prec_series, args.prec_inc)
         model_df.to_csv(filename)
         print(f'model_df saved to file {filename}')
     else:
         model_df = pd.read_csv(filename)
+    if args.plot:
+        plot_data(model_df)
 
     # Estimate parameter functions by estimating least squares coefficients for fitting the
     # functions as polynomials in x multiplied by sin/cos in t
@@ -81,7 +135,7 @@ def main():
         return np.log((x_p - x)**2 / (s_p - s))
 
     def kappa_f_hat(x, x_p, s, s_p, beta):
-        return np.log(np.abs(x_p - x) / np.abs(beta - x) / np.abs(s_p - s))
+        return np.log(np.abs(x_p - x) / np.abs(beta - x) / (s_p - s))
     
     n = model_df.shape[0] - 1
     x_data = np.array(model_df['x'])
@@ -102,10 +156,10 @@ def main():
         y_vec = np.zeros(n)
         for j in range(n):
             if param == 'kappa':
-                y_vec[j] = f_hat(x_data[j], s_data[j + 1], x_data[j], s_data[j + 1],
+                y_vec[j] = f_hat(x_data[j], x_data[j + 1], s_data[j], s_data[j + 1],
                     beta=X_mats['beta'][j, :] @ beta_hats['beta'])
             else:
-                y_vec[j] = f_hat(x_data[j], s_data[j + 1], x_data[j], s_data[j + 1])
+                y_vec[j] = f_hat(x_data[j], x_data[j + 1], s_data[j], s_data[j + 1])
             for i in range(n_X + 1):
                 x_pow = x_data[j] ** i
                 X_mat[j, (1 + 2*n_t) * i] = x_pow
@@ -123,15 +177,17 @@ def main():
     t_data = np.array(range(m + 1))
     x_inf = x_data.min()
     x_sup = x_data.max()
+    # s is in units of mm
+    # t is in units of hours
+    # x is in units of log(mm/h)
     delta_x = (x_sup - x_inf) / n
-    delta_t = 1 # Should this be in a time unit?
-    # delta_s = s_data.max() / z
-    delta_s = 1
-    print('delta_x', delta_x)
-    print('delta_t', delta_t)
-    print('delta_s', delta_s)
+    P = 24 * 365
+    delta_t = P / m
+    delta_s = args.delta_s
 
     def param_func(param, t, x=None):
+        # Input t is in units of days, so convert to hours
+        t = t * 24
         n_X, n_t, _ = param_info[param]
         X_vec = np.zeros((n_X + 1) * (1 + 2*n_t))
         for i in range(n_X + 1):
@@ -139,8 +195,7 @@ def main():
             x_pow = 1 if i == 0 else x ** i
             X_vec[(1 + 2*n_t) * i] = x_pow
             for k in range(n_t):
-                # Time period now equal to number of time steps
-                theta = 2*np.pi * t * (k + 1) / m
+                theta = 2*np.pi * t * (k + 1) / P
                 X_vec[(1 + 2*n_t) * i + 2*k + 1] = x_pow * np.sin(theta)
                 X_vec[(1 + 2*n_t) * i + 2*k + 2] = x_pow * np.cos(theta)
         return X_vec @ beta_hats[param]
@@ -150,6 +205,9 @@ def main():
         return np.exp(param_func('psi', t, x))
     def _K(t):
         return np.exp(param_func('kappa', t))
+
+    if args.plot:
+        plot_params(x_data, param_func)
     
     def peclet_num(x, t):
         return _K(t) * (_beta(t) - x) * delta_x / _v(t, x)
@@ -158,11 +216,12 @@ def main():
     G_mats = [np.zeros((n - 1, n - 1)) for _ in range(m)]
     H_mats = [np.zeros((n - 1, n - 1)) for _ in range(m)]
     for k in range(n - 1):
-        print(k, '/', n)
+        if k % 10 == 0:
+            print(k, '/', n)
         x_k = x_data[k]
-        x_km1 = x_data[k - 1] if k > 0 else 0
+        x_km1 = x_data[k - 1] if k > 0 else x_inf
         x_km0p5 = (x_km1 + x_k) / 2
-        x_kp1 = x_data[k + 1] if k < n - 2 else 0
+        x_kp1 = x_data[k + 1] if k < n - 2 else x_sup
         x_kp0p5 = (x_k + x_kp1) / 2
         for j in range(m):
             t = t_data[j + 1]
@@ -171,35 +230,41 @@ def main():
             pe_r = peclet_num(x_kp0p5, t)
             p_r = np.exp(-pe_r)
 
+            tp1 = t_data[j + 1]
+            pe_l_tp1 = peclet_num(x_km0p5, tp1)
+            p_l_tp1 = np.exp(pe_l_tp1)
+            pe_r_tp1 = peclet_num(x_kp0p5, tp1)
+            p_r_tp1 = np.exp(-pe_r_tp1)
+
             # Contributions from phi du/ds term
             f = lambda x: 1 /_v(t, x)
             if k > 0:
                 G_mats[j][k, k - 1] = delta_x**2 / delta_s * f(x_km1) \
                     / (p_l + 1) / (p_l + 2)
-                M_mats[j][k, k - 1, 0] = -G_mats[j][k, k - 1]
+                M_mats[j][k, k - 1, 0] = G_mats[j][k, k - 1]
             G_mats[j][k, k] = delta_x**2 / delta_s * f(x_k) \
                 * (1 / (p_l + 2) + 1 / (p_r + 2))
-            M_mats[j][k, k, 0] = -G_mats[j][k, k]
+            M_mats[j][k, k, 0] = G_mats[j][k, k]
             if k < n - 2:
                 G_mats[j][k, k + 1] = delta_x**2 / delta_s * f(x_kp1) \
                     / (p_r + 1) / (p_r + 2)
-                M_mats[j][k, k + 1, 0] = -G_mats[j][k, k + 1]
+                M_mats[j][k, k + 1, 0] = G_mats[j][k, k + 1]
 
             # Contributions from phi du/dt term
             f = lambda x: np.exp(-x) /_v(t, x)
             if k > 0:
                 H_mats[j][k, k - 1] = delta_x**2 / delta_t * f(x_km1) \
-                    / (p_l + 1) / (p_l + 2)
-                M_mats[j][k, k - 1, 1] = -H_mats[j][k, k - 1]
+                    / (p_l_tp1 + 1) / (p_l_tp1 + 2)
+                M_mats[j][k, k - 1, 1] = H_mats[j][k, k - 1]
             H_mats[j][k, k] = delta_x**2 / delta_t * f(x_k) \
-                * (1 / (p_l + 2) + 1 / (p_r + 2))
-            M_mats[j][k, k, 1] = -H_mats[j][k, k]
+                * (1 / (p_l_tp1 + 2) + 1 / (p_r_tp1 + 2))
+            M_mats[j][k, k, 1] = H_mats[j][k, k]
             if k < n - 2:
                 H_mats[j][k, k + 1] = delta_x**2 / delta_t * f(x_kp1) \
-                    / (p_r + 1) / (p_r + 2)
-                M_mats[j][k, k + 1, 1] = -H_mats[j][k, k + 1]
+                    / (p_r_tp1 + 1) / (p_r_tp1 + 2)
+                M_mats[j][k, k + 1, 1] = H_mats[j][k, k + 1]
 
-            # Contributions from phi du/dx term``
+            # Contributions from phi du/dx term
             if k > 0:
                 M_mats[j][k, k - 1, 2] = -pe_l / (p_l + 1)
             M_mats[j][k, k, 2] = pe_l / (p_l + 1) - pe_r / (p_r + 1)
@@ -213,10 +278,22 @@ def main():
             if k < n - 2:
                 M_mats[j][k, k + 1, 3] = 1/2
 
+    # Alternating negative solutions fixed due to incorrect signs in G, H matrices
+
+    # Mean, SD of output vary on delta_x but not delta_t, why?
+    
+    # Elements of A matrix corresponding to H are much smaller than those for M
+    # phi du/ds part of each M_mat term dominates
+    # phi du/dt, phi du/dx parts are negligible
+    # Peclet numbers are quite small (around 10^-5) due to _v being large (around 7000)
+    # Fixed due to typo in param estimation, now between 1 and 2
+    # Now the phi du/ds parts dominate (again)
+
     M_mats = [mat.sum(axis=2) for mat in M_mats]
     
     # Solve iteratively for each s
     u_vecs = [np.ones(m * (n - 1))]
+    start_time = datetime.now()
     for i in range(z):
         A_mat = np.zeros((m * (n - 1), m * (n - 1)))
         b_vec = np.zeros((m * (n - 1), 1))
@@ -226,49 +303,16 @@ def main():
             b_vec[start : end] = (G_mats[j] @ u_vecs[i][start : end]).reshape((n - 1, 1))
             A_mat[start : end, start : end] = M_mats[j]
             if j <= m - 2:
-                A_mat[start : end, start + n - 1 : end + n - 1] = H_mats[j + 1]
+                A_mat[start : end, start + n - 1 : end + n - 1] = -H_mats[j + 1]
         # Periodic boundary condition
         A_mat[(m - 1)*(n - 1) : m*(n - 1), 0 : n - 1] = -H_mats[0]
-        u_vecs.append(np.linalg.solve(A_mat, b_vec))
-        print()
-        print(u_vecs[i+1].reshape((m, n - 1)))
+        if i == 0:
+            np.savetxt('A_mat.csv', A_mat, delimiter=',')
+        u_vecs.append(spsolve(A_mat, b_vec))
+        if i == 5:
+            np.savetxt(f'u_{i+1}.csv', u_vecs[i+1].reshape((m, n - 1)), delimiter=',')
         print(i, u_vecs[i+1].mean(), u_vecs[i+1].std(), u_vecs[i+1].sum())
-
-    if not args.plot:
-        return
-
-    figure, axes = plt.subplots(3, 2)
-    axes = iter(axes.T.flatten())
-    axis = next(axes)
-    axis.plot(model_df['t'], model_df['s'], 'bo-')
-    axis.set_xlabel('t')
-    axis.set_ylabel('s')
-    
-    axis = next(axes)
-    axis.plot(model_df['t'], model_df['x'], 'ro-')
-    axis.set_xlabel('t')
-    axis.set_ylabel('x')
-
-    axis = next(axes)
-    axis.plot(model_df['t'], model_df['t_delta'], 'go-')
-    axis.set_xlabel('t')
-    axis.set_ylabel('t_delta')
-
-    axis = next(axes)
-    axis.plot(model_df['t'][:13], [param_func('beta', t) for t in range(13)], 'bo-')
-    axis.set_xlabel('t')
-    axis.set_ylabel('beta')
-
-    axis = next(axes)
-    axis.plot(model_df['t'][:13], [param_func('psi', t, x_data.mean()) for t in range(13)], 'ro-')
-    axis.set_xlabel('t')
-    axis.set_ylabel('psi')
-
-    axis = next(axes)
-    axis.plot(model_df['t'][:13], [param_func('kappa', t) for t in range(13)], 'go-')
-    axis.set_xlabel('t')
-    axis.set_ylabel('kappa')
-    plt.show()
+    print(f'Solving time: {datetime.now() - start_time}')
     
 if __name__ == '__main__':
     main()
